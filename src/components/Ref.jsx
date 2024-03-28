a52e3df0a373a01b8773172f5f505e101618cabf
daxchx <daxchx.v1@gmail.com>
2024-03-28 21:53:32 +0900

    add: useRefの使用例を追加 (#4)

---
+++ src/components/Ref.jsx
@@ -0,0 +1,14 @@
+import Case1 from "./ref/Case1";
+import Case2 from "./ref/Case2";
+import Case3 from "./ref/Case3";
+
+export default function Ref() {
+    return (
+    <div>
+        <span>useRef</span>
+        <Case1 />
+        <Case2 />
+        <Case3 />
+    </div>
+    )
+}
